3569b49a94434cffe212915301b731b9f714bbcb
Jonathan <zielinski@fastmail.com>
2021-04-25 23:28:34 +0200

    the day my computer crashed, so i had to competive programming for a raspberry pi.

---
+++ kattis/r2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// (r + x)/2 = res*2 
+//
+using namespace std;
+int main (void) {
+
+    int s, r;
+    cin >> r >> s;
+    cout << s*2-r;
+
+}
---
+++ log.md
@@ -0,0 +1,5 @@
+### 01
+
+Starting at 15.1 points on kattis,
+gonna work on the easy problems for
+a few days. Then do harder problems. 
