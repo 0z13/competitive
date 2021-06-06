edfc57c5bf71dc93aa15cac4a1e174aeb679fee4
Joe <zielinski@fastmail.com>
2021-06-07 00:57:24 +0200

    +1

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -105,10 +105,12 @@ pub fn find_disappeared_numbers(mut nums: Vec<i32>) -> Vec<i32> {
     let l = nums.len();
     let mut v: Vec<i32> = vec![];
     
-    for i in 0..=l {
+    for i in 1..=l {
         if !nums.contains(&(i as i32)) {
             v.push(i as i32)
         }
     }
     v
 }
+
+//fk
