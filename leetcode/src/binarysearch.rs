96c0140c93f2f554698172a9c18dd6acb095e7fd
Joe <zielinski@fastmail.com>
2021-06-14 21:38:31 +0200

    +1

--- leetcode/src/binarysearch.rs
+++ leetcode/src/binarysearch.rs
@@ -1,17 +1,7 @@
-fn main() {
-   println!("hi");
-
-
-    let v = vec![1,2,3,4];
-    println!("{}", search(v, 3));
-}
-
-
 pub fn search(nums: Vec<i32>, target: i32) -> i32 {
     let pos = nums.iter().position(|&t| t == target);
     match pos {
         Some(n) => return n as i32,
         None          => return -1,
     }
-
 }
