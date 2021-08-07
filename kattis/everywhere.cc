9f7d32d09f7092cab995424f0a4d904fabceca42
Joe <zielinski@fastmail.com>
2021-08-07 15:27:03 +0200

    +1

---
+++ kattis/everywhere.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+int main (void) {
+
+    int n;
+    cin >> n;
+    
+    for (auto i = 0; i< n; i++) {
+        int tst;
+        cin >> tst;
+        int counter = 0;
+        unordered_map<string, int) xs;
+        for (auto i = 0; i < tst; i++) {
+            string x;
+            cin >> x;
+            xs.insert(x, 0);
+            xs[x]++;
+            if (xs[x] == 0) 
+                counter++;
+        }
+        cout << counter << endl;
+    }
+
+}
