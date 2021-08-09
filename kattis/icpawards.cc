78619f7852092c863f5c2e4b2861483947078df6
Joe <zielinski@fastmail.com>
2021-08-09 19:10:30 +0200

    -1 ipcawards

--- kattis/icpawards.cc
+++ kattis/icpawards.cc
@@ -1,15 +1,36 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 using namespace std;
 int main (void) {
     
     int n;
+    int counter = 1;
     cin >> n;
+    unordered_map<string, int> m;
+
 
     for (auto i = 0; i < n; i++) {
-        string xs;
-        cin >> xs;
-        cout << xs;
+        string uni;
+        string team; 
+        cin >> uni;
+        cin >> team;
+
+
+        m.insert({uni, 0});
+        
+        m[uni] += 1;
+
+        if (m[uni] == 1) {
+            cout << uni << " " << team << "\n";
+            counter++;
+        }
+
+        if (counter >= 13) {
+            break;
+        }
+
+
     }
     return 0;
 
