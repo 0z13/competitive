827f3543d8d8e7096bdd555081460c8fa108b4f3
Joe <zielinski@fastmail.com>
2021-08-09 10:54:44 +0200

    -1 ipcawards

---
+++ kattis/icpawards.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main (void) {
+    
+    int n;
+    cin >> n;
+
+    for (auto i = 0; i < n; i++) {
+        string xs;
+        cin >> xs;
+        cout << xs;
+    }
+    return 0;
+
+}
