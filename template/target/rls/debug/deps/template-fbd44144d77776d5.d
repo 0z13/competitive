af76fc9b7b768a7f9eddd99b32eca96e7a1a3a02
Joe <zielinski@fastmail.com>
2021-08-28 22:49:00 +0200

    sus

--- template/target/rls/debug/deps/template-fbd44144d77776d5.d
+++
@@ -1,5 +0,0 @@
-/home/z/repos/competive/template/target/rls/debug/deps/template-fbd44144d77776d5.rmeta: src/main.rs
-
-/home/z/repos/competive/template/target/rls/debug/deps/template-fbd44144d77776d5.d: src/main.rs
-
-src/main.rs:
