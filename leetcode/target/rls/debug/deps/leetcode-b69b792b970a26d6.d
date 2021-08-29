337acdfcef4f39d41a0d0f54b1b96548a2f47d7c
Joe <zielinski@fastmail.com>
2021-08-29 06:50:35 +0200

    +1

--- leetcode/target/rls/debug/deps/leetcode-b69b792b970a26d6.d
+++ leetcode/target/rls/debug/deps/leetcode-b69b792b970a26d6.d
@@ -1,6 +1,5 @@
-/home/z/repos/competive/leetcode/target/rls/debug/deps/leetcode-b69b792b970a26d6.rmeta: src/main.rs src/sorting_the_sentence.rs
+/home/z/repos/competive/leetcode/target/rls/debug/deps/leetcode-b69b792b970a26d6.rmeta: src/main.rs
 
-/home/z/repos/competive/leetcode/target/rls/debug/deps/leetcode-b69b792b970a26d6.d: src/main.rs src/sorting_the_sentence.rs
+/home/z/repos/competive/leetcode/target/rls/debug/deps/leetcode-b69b792b970a26d6.d: src/main.rs
 
 src/main.rs:
-src/sorting_the_sentence.rs:
