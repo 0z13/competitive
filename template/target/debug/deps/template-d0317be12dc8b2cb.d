7426c877485750183a7765e19f0231b4b31c5291
Joe <zielinski@fastmail.com>
2021-09-12 17:09:21 +0200

    +1

--- template/target/debug/deps/template-d0317be12dc8b2cb.d
+++
@@ -1,5 +0,0 @@
-/home/z/repos/competive/template/target/debug/deps/template-d0317be12dc8b2cb.rmeta: src/main.rs
-
-/home/z/repos/competive/template/target/debug/deps/template-d0317be12dc8b2cb.d: src/main.rs
-
-src/main.rs:
