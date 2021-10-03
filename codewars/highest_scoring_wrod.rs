a3ff617357bc9e91372e8b496e90fe9152250bc5
nobody(and not in the badass action movie sense) <zielinski1337@yahoo.com>
2021-10-03 21:45:58 +0200

    +1

--- codewars/highest_scoring_wrod.rs
+++ codewars/highest_scoring_wrod.rs
@@ -1,10 +1,3 @@
-use std::{ascii::AsciiExt, cmp::max};
-
-fn main() {
-    println!("{:?}", high("man i need a taxi up to ubud"))
-}
-
-
 fn high(input: &str) -> &str {
     let mut mx = 0;
     let mut word = "";
@@ -28,12 +21,3 @@ fn score(i: &str) -> i32 {
 }
 
 
-fn in_array(arr_a: &[&str], arr_b: &[&str]) -> Vec<String> {
-    let mut a = arr_a
-        .into_iter()
-        .filter(|word| !arr_b.contains(word))
-        .map(|word| word.to_string())
-        .collect::<Vec<String>>();
-    a.sort();
-    a
-}
