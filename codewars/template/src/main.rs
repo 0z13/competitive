efa1f751f0625231220eb95e03c54fc59766509f
nobody(and not in the badass action movie sense) <zielinski1337@yahoo.com>
2021-10-05 13:40:08 +0200

    +1

--- codewars/template/src/main.rs
+++ codewars/template/src/main.rs
@@ -1,6 +1,7 @@
 use std::{ascii::AsciiExt, cmp::max};
 
 fn main() {
+    println!("{}", rgb(-20, 275, 125));
     println!("{:?}", high("man i need a taxi up to ubud"))
 }
 
@@ -36,4 +37,16 @@ fn in_array(arr_a: &[&str], arr_b: &[&str]) -> Vec<String> {
         .collect::<Vec<String>>();
     a.sort();
     a
+}
+
+fn rgb(r: i32, g: i32, b: i32) -> String {
+    let  mut xs =[r,g,b];
+    for i in xs.iter_mut() {
+        if *i < 0 {
+            *i = 0;
+        } else if *i > 255 {
+            *i = 255;
+        }
+    }
+    format!("{:0width$X}{:0width$X}{:0width$X}", xs[0], xs[1], xs[2], width=2)
 }
