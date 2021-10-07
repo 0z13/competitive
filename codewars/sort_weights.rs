4b40cdfee7a3d5b48bd045f6891cdc132ed0b078
nobody(and not in the badass action movie sense) <zielinski1337@yahoo.com>
2021-10-07 16:38:47 +0200

    +1

--- codewars/sort_weights.rs
+++ codewars/sort_weights.rs
@@ -2,7 +2,8 @@ use std::{ascii::AsciiExt, cmp::max, fmt::format, slice::Split};
 
 fn main() {
     println!("{}", digit_sum(123));
-    println!("{}", order_weight(&"2000 10003 1234000 44444444 9999 11 11 22 123"));
+    println!("{}", order_weight(&"2000 10003 1234000 44444444 9999 11 11 22
+123"));
     println!("{}", order_weight(&"103 123 4444 99    2000"));
 }
 
@@ -59,3 +60,4 @@ fn order_weight(s: &str) -> String {
     }
     res.trim_end().to_string()
 }
+
