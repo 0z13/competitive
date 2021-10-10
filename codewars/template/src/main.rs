89d7134c5dbbfcee38cca49ed75bbef88c802b85
nobody(and not in the badass action movie sense) <zielinski1337@yahoo.com>
2021-10-10 19:28:45 +0200

    000

--- codewars/template/src/main.rs
+++ codewars/template/src/main.rs
@@ -1,26 +1,6 @@
-use std::{ascii::AsciiExt, cmp::max, fmt::format, slice::Split};
+
 
 fn main() {
-    println!("{}", xo(&"ooom"));
+    println!("{}", "hello");
 }
 
-fn xo(string: &'static str) -> bool {
-    string
-        .chars()
-        .filter(|x| x.to_ascii_lowercase() == 'o')
-        .count()
-        == string
-            .chars()
-            .filter(|x| x.to_ascii_lowercase() == 'x')
-            .count()
-}
-
-#[test]
-fn returns_expected() {
-    assert_eq!(xo("xo"), true);
-    assert_eq!(xo("Xo"), true);
-    assert_eq!(xo("xxOo"), true);
-    assert_eq!(xo("xxxm"), false);
-    assert_eq!(xo("Oo"), false);
-    assert_eq!(xo("ooom"), false);
-}
