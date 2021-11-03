47027114eaae010d4dc266a6ab6e87b6a84fbda9
nobody(and not in the badass action movie sense) <zielinski1337@yahoo.com>
2021-11-03 23:50:52 +0100

    woops'

---
+++ kattis/eatingouit.rs
@@ -0,0 +1,111 @@
+#![allow(dead_code)]
+#![allow(unused)]
+#![allow(unused_imports)]
+use std::cmp::{max, min, Reverse};
+use std::cmp::Ordering::{Equal,Less,Greater};
+use std::collections::btree_set::Intersection;
+use std::collections::{HashMap, BTreeMap, HashSet};
+use std::io::{self, prelude::*};
+use std::iter::FromIterator;
+use std::str;
+use std::f64::consts::PI;
+use std::fmt::Display;
+struct Scanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitWhitespace<'static>,
+}
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_whitespace(),
+        }
+    }
+    fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_whitespace())
+            }
+        }
+    }
+    // annoying EOF parsing
+    fn read_str(&mut self) -> Option<String>{
+        // should probably trim stuff
+        let mut s = String::new();
+        let mut si = self.reader.read_line(&mut s);
+        if s.eq("\n") {
+            return None
+        }
+        if s.eq("EOF\n") {
+            return None
+        }
+        if s.eq("EOF") {
+            return None
+        }
+        match si {
+            // EOF
+            Ok(0) => None,
+            _     => Some(s)
+        }
+  }
+}
+
+
+fn unique_digits(x: i64) -> bool {
+    let mut counter =0;
+    let mut set = HashSet::new();
+    let mut num = x;
+
+    while (num > 0) {
+        set.insert(num % 10);
+        num = num / 10;
+        counter += 1;
+    }
+
+    counter == set.len()
+
+}
+
+fn div_by_self(x: i64) -> bool {
+    let mut num = x;
+
+    while (num > 0) {
+        if num % 10 == 0 {
+            return false
+        }
+        if (x % (num % 10) != 0) {
+            return false;
+        }
+        num = num / 10;
+    }
+    true
+}
+
+//
+fn solve<R, W>(scan: &mut Scanner<R>, w: &mut W)
+where
+    R: BufRead,
+    W: Write,
+{
+    let m = scan.token::<i32>();
+    let a = scan.token::<i32>();
+    let b = scan.token::<i32>();
+    let c = scan.token::<i32>();
+    a+b+c > 2*m
+}
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = Scanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+    solve(&mut scan, &mut out);
+}
