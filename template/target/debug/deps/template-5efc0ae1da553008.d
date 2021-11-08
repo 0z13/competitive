60cd6db715c155dd883f2c981629701512305863
nobody(and not in the badass action movie sense) <zielinski1337@yahoo.com>
2021-11-08 23:40:20 +0100

    +1

---
+++ template/target/debug/deps/template-5efc0ae1da553008.d
@@ -0,0 +1,5 @@
+/home/jo/repos/competitive/template/target/debug/deps/template-5efc0ae1da553008: src/main.rs
+
+/home/jo/repos/competitive/template/target/debug/deps/template-5efc0ae1da553008.d: src/main.rs
+
+src/main.rs:
