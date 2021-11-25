3ce006b75cf1d9ff1bdf1910a5eb7d5b0427c306
Jonathan Zielinski <zielinski1337@yahoo.com>
2021-11-25 17:29:21 +0100

    +1

---
+++ kattis/chopin.rs
@@ -0,0 +1,144 @@
+#![allow(dead_code)]
+#![allow(unused)]
+#![allow(unused_imports)]
+use std::cmp::Ordering::{Equal, Greater, Less};
+use std::cmp::{max, min, Reverse};
+use std::collections::btree_set::Intersection;
+use std::collections::{BTreeMap, HashMap, HashSet};
+use std::f64::consts::PI;
+use std::fmt::Display;
+use std::io::{self, prelude::*};
+use std::iter::FromIterator;
+use std::str;
+use crate::Instr::{DefineExpr,EvalExpr};
+
+struct Scanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitWhitespace<'static>,
+}
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_whitespace(),
+        }
+    }
+    fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_whitespace())
+            }
+        }
+    }
+    // annoying EOF parsing
+    fn read_str(&mut self) -> Option<String> {
+        let mut s = String::new();
+        let mut si = self.reader.read_line(&mut s);
+        if s.eq("\n") {
+            return None;
+        }
+        if s.eq("EOF\n") {
+            return None;
+        }
+        if s.eq("EOF") {
+            return None;
+        }
+        match si {
+            // EOF
+            Ok(0) => None,
+            _ => Some(s.trim_end().to_string()),
+        }
+    }
+}
+type Op = char;
+
+
+#[derive(Debug)]
+enum Instr {
+    DefineExpr(i32, String),
+    EvalExpr(Op, String, String),
+}
+
+fn parse(xs:Vec<Vec<String>>) -> Vec<Instr> {
+    let mut res:Vec<Instr> = vec![];
+    
+    for v in xs {
+        if v[0].eq(&String::from("define")) {
+            let x:i32 = v[1].parse().unwrap();
+            let a = DefineExpr(x,v[2].clone());
+            res.push(a)
+        }
+        if v[0].eq(&String::from("eval")) {
+            let op = v[2].parse::<char>().unwrap();
+            let a = EvalExpr(op, v[1].clone(), v[3].clone());
+            res.push(a);
+        }
+    }
+    res
+}
+
+
+
+
+
+
+fn solve<R: BufRead, W: Write>(scan: &mut Scanner<R>, w: &mut W) {
+        let mut s = scan.read_str();
+        let mut counter = 0;
+        let mut flag = false;
+        while s.is_some() {
+                counter += 1;
+                let mut res = format!("Case {}: ", counter);
+                if let Some(z) = s {
+                       let m: Vec<&str> = z.split(' ').collect();
+                       match m[0] {
+                           "Ab" =>
+                           {res.push_str("G#");}
+                           "G#" => 
+                           {res.push_str("Ab");}
+                           "D#" =>
+                           {res.push_str("Eb");}
+                           "Eb" =>
+                           {res.push_str("D#");}
+                           "A#" =>
+                           {res.push_str("Bb");}
+                           "Bb" =>
+                           {res.push_str("A#");}
+                           "C#" =>
+                           {res.push_str("Db");}
+                           "Db" =>
+                           {res.push_str("C#");}
+                           "Db" =>
+                           {res.push_str("G#");}
+                           "F#" => 
+                           {res.push_str("Gb");}
+                           "Gb" => 
+                           {res.push_str("F#");}
+                           _ => {res.push_str("UNIQUE");
+                                 flag = true;
+                           }
+                        }
+                        if !flag {
+                            res.push_str(format!(" {}", m[1]).as_str());
+                        }
+                 }
+             writeln!(w,"{}",res);
+             s = scan.read_str();
+         }
+}
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = Scanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+    solve(&mut scan, &mut out);
+}
