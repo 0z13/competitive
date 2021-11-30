d41f0369392b5112939feffa9947a5debfc308d5
Jonathan Zielinski <zielinski1337@yahoo.com>
2021-11-30 23:14:08 +0100

    zz

---
+++ template/target/debug/deps/template-c49069de183b414c.d
@@ -0,0 +1,5 @@
+/home/z/repos/competitive/template/target/debug/deps/template-c49069de183b414c.rmeta: src/main.rs
+
+/home/z/repos/competitive/template/target/debug/deps/template-c49069de183b414c.d: src/main.rs
+
+src/main.rs:
